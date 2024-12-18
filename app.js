c72f91222731690cbed8093ae5a5115612791eff
sufyaanmalik <sufyaanmaalik@gmail.com>
2024-12-18 09:23:59 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// CHapter # 2
+
+// Question # 1
+
+var username = "";
+
+// Question # 2
+
+let myName = "Muhammad Sufyan Malik" + "<br><br>";
+
+document.write(myName)
+
+
+// Question # 3
+
+let message = "Hello World";
+alert(message);
+
+// Question # 4
+
+let name = "Name: Sufyan Malik";
+let age = "Age: 24";
+let bio = "Bio: A professional Web developer";
+
+alert(name);
+alert(age);
+alert(bio);
+
+
+// Question # 5
+
+let intro = name + age + bio;
+document.write(intro);
